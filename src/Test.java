394a3b34d6f19c6730c4050bdf9b4ffe20387d75
mmmdamin <mmmdamin@gmail.com>
2015-07-27 08:49:16 +0430

    add multiply

--- src/Test.java
+++ src/Test.java
@@ -21,4 +21,22 @@ class Test
             System.out.println("Sum: Failed, expected = " + expected + ", result = " + result);
         }
     }
+
+    private static void testMul()
+    {
+        int a = 5;
+        int b = 6;
+        int expected = 30;
+        BasicMath bm = new BasicMath();
+        int result = bm.mul(a, b);
+        if (result == expected)
+        {
+            System.out.println("Mul: OK");
+        }
+        else
+        {
+            System.out.println("Mul: Failed, expected = " + expected + ", result = " + result);
+        }
+    }
+
 }
